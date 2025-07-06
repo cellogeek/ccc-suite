e4f0ff2e2dd38ef2dd241a37198ce5230b9e7cb7
cellogeek <noahjlittlejohn@gmail.com>
2025-07-06 07:42:25 -0500

    Update page.tsx

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -48,7 +48,7 @@ export default function LibraryPage() {
       const userPresentations = await supabaseService.getUserPresentations(session.user.id);
 // Filter out any presentations that don't have an ID
 const validPresentations = userPresentations.filter(p => p.id);
-setPresentations(validPresentations);
+setPresentations(validPresentations as PresentationData[]);
     } catch (error) {
       console.error('Error loading presentations:', error);
     } finally {
